9b0b50900b9cd5951681e224b7fa811a8b7ce6f7
LipigaArunachalam <lipigava.22it@kongu.edu>
2026-03-26 16:16:41 +0530

    cors updation

--- frontend/src/shared/utils/rtkQuery/baseApi.js
+++ frontend/src/shared/utils/rtkQuery/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_API_URL}/api`,
+    baseUrl: `${import.meta.env.VITE_API_URL}`,
     credentials: 'include',
   }),
   tagTypes: ['Sellers', 'customers', 'Products'],
