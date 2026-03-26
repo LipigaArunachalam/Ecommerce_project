297a0c1fdc58ef0e6089c54bf17af857c70281e1
LipigaArunachalam <lipigava.22it@kongu.edu>
2026-03-26 15:54:57 +0530

    cors updation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-  origin: "https://ecommerce-project-hazel-tau.vercel.app/",
+  origin: [
+    "http://localhost:5173",
+    "https://ecommerce-project-hazel-tau.vercel.app"
+  ],
   credentials: true,
 });
   app.useGlobalPipes(
